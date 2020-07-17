82aeca1ffd822fb096ee1f7b21618562abb3241c
uniqsy <m15265723639@163.com>
2020-07-17 21:21:05 +0800

    HelloWorld项目添加注释

--- Uniqsy/source/0717/HelloWorld/main.go
+++ Uniqsy/source/0717/HelloWorld/main.go
@@ -1,7 +1,6 @@
 package main
 
 import (
-	"fmt"
 	"github.com/gin-gonic/gin"
 	"math/rand"
 	"net/http"
@@ -12,10 +11,12 @@ func main() {
 	r := gin.Default()
 
 	r.GET("/hw", func(c *gin.Context) {
+		//返回HelloWorld内容
 		c.String(http.StatusOK, "Hello World")
 	})
 
 	r.GET("/rand/:seed", func(c *gin.Context) {
+		//根据提供的seed随机生成大数
 		seedStr := c.Param("seed")
 		seed, err := strconv.ParseInt(seedStr, 10, 64)
 		if err != nil {
@@ -30,33 +31,43 @@ func main() {
 	})
 
 	r.POST("/image", func(c *gin.Context) {
+		//上传图片并检查图片类型
+
+		//获取图片名
 		fileName := c.PostForm("name")
+		//获取图片文件
 		file, err := c.FormFile("image")
-		fmt.Println(fileName)
 		if err != nil {
 			c.JSON(http.StatusInternalServerError, gin.H{
 				"error": err,
 			})
+			return
 		}
 
+		//打开图片，
 		f, err := file.Open()
 		if err != nil {
 			c.JSON(http.StatusInternalServerError, gin.H{
 				"error": err,
 			})
+			return
 		}
-
+		//读取图片的前512个字节，用于鉴定文件是否为图片
 		buffer := make([]byte, 512)
 		_, err = f.Read(buffer)
 		if err != nil {
 			c.JSON(http.StatusInternalServerError, gin.H{
 				"error": err,
 			})
+			return
 		}
+		f.Close()
 
+		//鉴定文件类型
 		fileType := http.DetectContentType(buffer)
 		if fileType == "image/jpeg" || fileType == "image/jpg" ||
 			fileType == "image/gif" || fileType == "image/png" {
+			//保存图片
 			err = c.SaveUploadedFile(file, fileName)
 			if err != nil {
 				c.JSON(http.StatusInternalServerError, gin.H{
@@ -66,6 +77,7 @@ func main() {
 				c.String(http.StatusOK, "upload successfully")
 			}
 		} else {
+			//如果不是图片则不保存
 			c.String(http.StatusOK, "file is not image")
 		}
 
