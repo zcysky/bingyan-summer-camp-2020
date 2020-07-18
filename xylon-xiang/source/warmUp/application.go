bc774aa612546f15f6b41ab3b430fb0d63ba119c
xylon-xiang <3564578413@qq.com>
2020-07-18 21:26:21 +0800

    start the project

--- xylon-xiang/source/warmUp/application.go
+++ xylon-xiang/source/warmUp/application.go
@@ -2,13 +2,49 @@ package main
 
 import (
 	"github.com/labstack/echo"
+	"github.com/labstack/echo/middleware"
+	"warmUp/config"
 	"warmUp/controller"
 )
 
-func main() {
-	e := echo.New()
+var e *echo.Echo
 
-	_ = controller.DeleteUserController(e)
+func main() {
+	e = echo.New()
+
+	g := e.Group("/user")
+
+	g.Use(middleware.JWTWithConfig(middleware.JWTConfig{
+		SigningMethod: config.Config.JWT.JWTSigningMethod,
+		SigningKey:    []byte(config.Config.JWT.JWTSecret),
+		//Skipper: SkipperFunc,
+	}))
+
+	controller.LoginController(e)
+
+	controller.UpdateUserInfoController(g)
+
+	controller.DeleteUserController(g)
+
+	controller.GetUserInfoController(g)
 
 	e.Logger.Fatal(e.Start(":1323"))
 }
+
+//func SkipperFunc(ctx echo.Context) bool {
+//
+//	matched, err := regexp.Match("/user/.*?password=.*$", []byte(ctx.Request().RequestURI))
+//	if err != nil{
+//		_ =  ctx.String(http.StatusInternalServerError, "regexp error")
+//	}
+//
+//	filter1 := (ctx.Request().Method == http.MethodGet) && matched
+//
+//	if filter1 {
+//		controller.LoginController(e)
+//		return true
+//	}
+//
+//
+//	return false
+//}
