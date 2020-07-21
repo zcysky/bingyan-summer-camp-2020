a3838816d9bc907bede7d495753f2238dd476073
TobyZT <toby771293912@outlook.com>
2020-07-21 16:45:22 +0800

    管理员查询成员数据请求的handler

--- TobyZT/Source/account/controller/query.go
+++ TobyZT/Source/account/controller/query.go
@@ -5,21 +5,69 @@ package controller
 import (
 	"account/model"
 	"github.com/gin-gonic/gin"
+	"log"
 	"net/http"
 )
 
 func QueryAllUsers(c *gin.Context) {
+	tokenStr := c.Request.Header.Get("Authorization")
+	if tokenStr == "" || len(tokenStr) < 7 {
+		failUpdate(c, http.StatusUnauthorized, "Unauthorized")
+		return
+	}
+	_, admin, valid, err := ParseToken(tokenStr[7:])
+	if err != nil {
+		log.Println(err)
+	}
+	if !valid || !admin {
+		failUpdate(c, http.StatusUnauthorized, "Unauthorized")
+		return
+	}
+
 	var form model.QueryForm
-	err := c.BindJSON(&form)
+	err = c.BindJSON(&form)
 	if err != nil {
 		failQuery(c, http.StatusBadRequest, err.Error())
 		return
 	}
-	users, err := model.QueryAll(form.Limit, form.Page)
-	// ------ Not complete --------
+	users, total, err := model.QueryAll(form.Limit, form.Page)
+	if err != nil {
+		failQuery(c, http.StatusBadRequest, err.Error())
+		return
+	}
+	var res []gin.H
+	for _, user := range users {
+		res = append(res, gin.H{
+			"userid":   user.UserID,
+			"username": user.Username,
+			"phone":    user.Phone,
+			"email":    user.Email,
+		})
+	}
+	c.JSON(http.StatusOK, gin.H{
+		"message": "success",
+		"status":  http.StatusOK,
+		"total":   total,
+		"page":    form.Page,
+		"limit":   form.Limit,
+		"users":   res,
+	})
 }
 
 func QueryOne(c *gin.Context) {
+	tokenStr := c.Request.Header.Get("Authorization")
+	if tokenStr == "" || len(tokenStr) < 7 {
+		failUpdate(c, http.StatusUnauthorized, "Unauthorized")
+		return
+	}
+	_, admin, valid, err := ParseToken(tokenStr[7:])
+	if err != nil {
+		log.Println(err)
+	}
+	if !valid || !admin {
+		failUpdate(c, http.StatusUnauthorized, "Unauthorized")
+		return
+	}
 	userid := c.Param("userid")
 	user, err := model.QueryOne(userid)
 	if err != nil {
